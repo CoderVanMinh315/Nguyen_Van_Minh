27fbf8fef92dc8e308d810c91dae2100ebe35ceb
CoderVanMinh315 <vanminh3105@gmai.com>
2025-03-09 09:57:17 +0700

    Them file toan.c

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
-#include<stdio.h>
-
+﻿#include<stdio.h>
+int Cong(int a, int b);
+int Tru(int a, int b);
+int Nhan(int a, int b);
+int Chia(int a, int b);
 void main() {
-	printf("Hello world");
+	printf("Hello world\n");
+	int x = Cong(56, 2);
+	printf("Ket qua phep cong x: %d\n", x);
+	int y = Tru(56, 2);
+	printf("Ket qua phep tru x: %d\n", y);
+	int z = Nhan(56, 2);
+	printf("Ket qua phep nhan x: %d\n", z);
+	int t = Chia(56, 2);
+	printf("Ket qua phep chia x: %d\n", t);
+
 }
---
+++ toan.c
@@ -0,0 +1,30 @@
+﻿int Cong(int a, int b)
+{
+	int ketqua = 0;
+	ketqua = a + b;
+	return ketqua;
+}
+int Tru(int a, int b)
+{
+	int ketqua = 0;
+	ketqua = a - b;
+	return ketqua;
+}
+int Nhan(int a, int b)
+{
+	int ketqua = 0;
+	ketqua = a * b;
+	return ketqua;
+}
+int Chia(int a, int b)
+{
+	int ketqua = 0;
+	{
+		if (b == 0)  // Kiểm tra chia cho 0
+		{
+			printf("Lỗi: Không thể chia cho 0\n");
+			return 0; // Trả về 0 hoặc có thể xử lý khác tùy theo yêu cầu
+		}
+		return ketqua = a / b; // Thực hiện phép chia nguyên
+	}
+}
