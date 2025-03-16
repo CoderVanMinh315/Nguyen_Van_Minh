31c6c7117f9e51257b78dac40a3af583d9ae4983
CoderVanMinh315 <vanminh3105@gmai.com>
2025-03-16 09:14:21 +0700

    So luong ky tu

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
-﻿#include<stdio.h>
-void main()
-{
-	int mang[5] = { 5,2,3,49,5 };
-	int tong = 0;
-	int max = mang[0];
-	for (int i = 0; i < 5; i++)
-	{
-		if (mang[i] >= max)
-		{
-			max = mang[i];
-		}
-	}
-	printf("%d", max);
+﻿#include <stdio.h>
+
+int main() {
+    char str[] = "Hello";
+    int count = 0;
+
+    while (str[count] != '\0') {
+        count++;
+    }
+
+    printf("Chuoi: %s\n", str);
+    printf("So luong ki tu: %d\n", count);
+
+    return 0;
 }
