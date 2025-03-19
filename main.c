b901fc385c6ea16996e26187a0d42c62d02bb651
CoderVanMinh315 <vanminh3105@gmai.com>
2025-03-19 21:41:13 +0700

    Chuyen chu thuong thanh in hoa

--- main.c
+++ main.c
@@ -1,20 +1,14 @@
-﻿#include <stdio.h>
-
-// Function to count the number of space characters in a string
-int count_spaces(const char* str) {
-    int count = 0;
-    while (*str) {
-        if (*str == ' ') {
-            count++;
-        }
-        str++;
-    }
-    return count;
+﻿#include<stdio.h>
+void to_uppercase(char* str) {
+	while (*str) {
+		if (*str >= 'a' && *str <= 'z') {
+			*str = *str - ('a' - 'A');
+		}
+		str++;
+	}
 }
-
 int main() {
-    char str[] = "Hello MINH. MINH";
-    int space_count = count_spaces(str);
-    printf("Number of space characters in the string: %d\n", space_count);
-    return 0;
+	char str[] = "Hello world! Van Minh day";
+	to_uppercase(str);
+	printf("Uppercase string: %s\n", str);
 }
